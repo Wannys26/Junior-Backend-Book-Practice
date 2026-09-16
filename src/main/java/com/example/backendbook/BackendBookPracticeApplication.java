ee8d729a6f4b4295c485dd928ed32766455ab352
fkgrkyr <fkgrkyr@gmail.com>
2026-09-17 00:14:00 +0900

    chore: Spring Boot 실습 프로젝트 초기 설정

---
+++ src/main/java/com/example/backendbook/BackendBookPracticeApplication.java
@@ -0,0 +1,11 @@
+package com.example.backendbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBookPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBookPracticeApplication.class, args);
+    }
+}
